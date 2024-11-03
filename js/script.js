d35d7aa82604b66dd725ed771a264c1ae2317497
Lasha-Shamugia <lashashamugia89@gmail.com>
2024-11-03 21:55:46 +0100

    done

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
 let inputElement = document.getElementById("send-el");
-
 inputElement.addEventListener("click", function () {
-  alert("Thank you for share your opinion!");
+  alert("Thank You for Sharing Your Opinion!");
 });
 
-// let plusButton = document.getElementById("plus-btn");
-
-// plusButton.addEventListener("click", function() {
-//   document.
-// })
+let signButton = document.getElementById("sign-button");
+signButton.addEventListener("click", function () {
+  signButton.classList.toggle("newClassBtn");
+});
 
 let userElement = document.getElementById("subject");
-
 userElement.addEventListener("keydown", function () {
   userElement.classList.add("newClass");
 });
 
 let user1Element = document.getElementById("lname");
-
 user1Element.addEventListener("keydown", function () {
   user1Element.classList.add("newClassLname");
 });
@@ -26,3 +22,10 @@ let user2Element = document.getElementById("fname");
 user2Element.addEventListener("keydown", function () {
   user2Element.classList.add("newClassFname");
 });
+
+
+let buttonElement = document.getElementById("order-button");
+buttonElement.addEventListener("click", function () {
+  alert("Please visit our menu and choose a product to order it");
+});
+
